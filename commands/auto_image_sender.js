1f22a7b2e93757517355d4451999e4a059ed73fb
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2024-01-09 19:04:19 +0530

    Update auto_image_sender.js

--- commands/auto_image_sender.js
+++ commands/auto_image_sender.js
@@ -13,7 +13,7 @@ const { tlang,cmd } = require('../lib')
 const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
 const Config = require('../config')
 const axios = require('axios')
-// Put here your url with modified words and urls
+// Auto image code
 const url = 'https://gist.github.com/vajirabot1/9dd585014e2742484992c93d008531d4/raw'
 
 /**
@@ -30,5 +30,40 @@ cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
 }
 })
 
+//---------------------------------------------------------------------------
 
- 
+// Auto voice code
+const url = 'https://gist.github.com/vajirabot1/8408dce754dc57efaecbdd100ac93ddd/raw'
+
+/**
+ cmd({
+            pattern: "bgmnsew",
+            category: "owner",
+            use: '',
+        },
+**/ 
+cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
+  let { data } = await axios.get(url)
+  for (vr in data){
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
+}
+})
+
+//---------------------------------------------------------------------------
+
+// Auto sticker code
+const url = 'https://gist.github.com/vajirabot1/377d6bdff0009b518cb858c57f36a8a0/raw'
+
+/**
+ cmd({
+            pattern: "autosticker",
+            category: "owner",
+            use: '',
+        },
+**/ 
+cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
+  let { data } = await axios.get(url)
+  for (vr in data){
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) await Void.sendMessage(citel.chat,{sticker: { url : data[vr]},package: 'made by vajira'},{ quoted: citel })   
+}
+})
