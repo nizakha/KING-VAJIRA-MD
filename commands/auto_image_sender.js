c42a77e8e19e3551c19d618a1761969119bdfb77
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2024-01-09 15:35:44 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/auto_image_sender.js
+++ commands/auto_image_sender.js
@@ -10,6 +10,7 @@
 **/
 
 const { tlang,cmd } = require('../lib')
+const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
 const Config = require('../config')
 const axios = require('axios')
 // Put here your url with modified words and urls
@@ -17,7 +18,7 @@ const url = 'https://gist.github.com/vajirabot1/9dd585014e2742484992c93d008531d4
 
 /**
  cmd({
-            pattern: "bgmnsew",
+            pattern: "autosticker",
             category: "owner",
             use: '',
         },
@@ -25,6 +26,9 @@ const url = 'https://gist.github.com/vajirabot1/9dd585014e2742484992c93d008531d4
 cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
   let { data } = await axios.get(url)
   for (vr in data){
- if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{image: { url : data[vr]},mimetype: 'image/mpeg',ptt:true},{quoted:citel})   
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) await Void.sendMessage(citel.chat,{image: { url : data[vr]},package: 'made by vajira'},{ quoted: citel })   
 }
 })
+
+
+ 
